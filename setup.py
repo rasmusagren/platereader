2e8f424fd9179489520c33e6ce489d507b5b2a64
Rasmus Ågren <rasmus.agren@scilifelab.se>
2015-11-05 11:14:57 +0100

    pip package

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='PlateReader',
       version='0.1',
       description='Software for counting and quantifying colonies on nutrient plates',
       url='http://github.com/rasmusagren/platereader',
       author='Rasmus Agren',
       author_email='rasmus@alembic.se',
-      license='MIT',
+      license='GNU General Public License v2.0',
       packages=['platereader'],
+      include_package_data=True,
+      install_requires=[
+          'numpy',
+          'cv2',
+          'matplotlib',
+          'pygtk'],
       zip_safe=False)
